40ef04e69e763543c04ccf5282be68020e0702b5
Elman Krinker <ekrinker@bdr-itwv-mongo-3.dev.uspto.gov>
2018-01-05 16:44:19 -0500

    changes off mongo-3

--- app/claim_similarity_evaluation.py
+++ app/claim_similarity_evaluation.py
@@ -32,31 +32,33 @@ PATI_DATA = app.config['PATI_DATA']
 findspark.init(FINDSPARK_INIT)
 # conf = pyspark.SparkConf().setAppName('claim_similarity')
 # sc = pyspark.SparkContext(conf=conf).getOrCreate()
-spark = SparkSession.builder.appName('claim_similarity').enableHiveSupport().getOrCreate()
+spark = SparkSession.builder.appName('claim_similarity').config("spark.driver.maxResultSize", "4g").enableHiveSupport().getOrCreate()
 df_pati_data = spark.read.parquet('hdfs://bdr-itwv-mongo-3.dev.uspto.gov:54310/tmp/PATI_data/data')
-df_pati_data = df_pati_data.sample(withReplacement=False, fraction=0.05, seed=123)
+df_pati_data = df_pati_data.sample(withReplacement=False, fraction=0.001, seed=123)
 
-# print('# of rows: ', df_pati_data.count())
-# print(df_pati_data.show())
+print('# of rows: ', df_pati_data.count())
+print('PATI Data')
+print(df_pati_data.show())
 
 df_pati_clm_data = df_pati_data[df_pati_data['type'] == "CLM"]
+print('Claim Only Data')
 print(df_pati_clm_data.show())
+
+df_pati_clm_txt = df_pati_clm_data.drop('text')
+print('Claim Text')
+print(df_pati_clm_txt)
 
 
 def process_huge_claims(claim_txt):
     claim_corpus = []
     # corpus, with no pre-processing to retrieve the original documents.
     documents = []
-    # for index_val, txt_val in claim_txt.iteritems():
-    #     # print(txt_val)
-    #     splitup_txt = [s.strip() for s in txt_val.split("***")]
-    #     for txt in splitup_txt:
-    #         claim_corpus.append(pre_process(txt))
-    #         documents.append(txt)
-    blah = claim_txt.toPandas().values
-    # status_paths_with_art_class = claim_txt.flatMap(lambda x: [x['text']]).collect()
-
-    # claim_txt.collect().foreach(claim_corpus.append())
+    # convert claim text rdd into list of claims
+    claims = claim_txt.toPandas().values
+    print('*********************extracted claim text*********************************')
+    for claim in claims:
+        claim_corpus.append(pre_process(claim))
+	documents.append(claim)
 
     return claim_corpus, documents
 
@@ -66,12 +68,11 @@ start = time()
 # df = pd.read_csv("data/pati_data.csv")
 # claim_txts = df['CLAIM_TXTS']
 # claim_txt_corpus, original_corpus = process_claims(claim_txts)
-claim_txts = df_pati_clm_data
-claim_txt_corpus, original_corpus = process_huge_claims(claim_txts)
+claim_txt_corpus, original_corpus = process_huge_claims(df_pati_clm_txt)
 print('Took %.2f seconds to load PATI data.' % (time() - start))
 
-tableList = [x["text"] for x in df_pati_clm_data.rdd.collect()]
-
+#tableList = [x["text"] for x in df_pati_clm_data.rdd.collect()]
+#print(tableList)
 
 # spark.stop()
 
--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
-    FINDSPARK_INIT = os.environ.get('FINDSPARK_INIT') or 'C:/spark/spark-2.2.0-bin-hadoop2.7/'
+    FINDSPARK_INIT = os.environ.get('FINDSPARK_INIT') or '/usr/hdp/current/spark2-client/'
     PATI_DATA = os.environ.get('PATI_DATA') or 'hdfs://bdr-itwv-mongo-3.dev.uspto.gov:54310/tmp/PATI_data/data'
 
