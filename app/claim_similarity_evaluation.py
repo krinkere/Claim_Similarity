d854c246b02600ae7ded0c0c4218ce9b3bb499be
Elman (Al) Krinker <elman.krinker@uspto.gov>
2018-01-05 15:11:31 -0500

    more perf hacks

--- app/claim_similarity_evaluation.py
+++ app/claim_similarity_evaluation.py
@@ -39,34 +39,48 @@ df_pati_data = df_pati_data.sample(withReplacement=False, fraction=0.05, seed=12
 # print('# of rows: ', df_pati_data.count())
 # print(df_pati_data.show())
 
-print("*****")
 df_pati_clm_data = df_pati_data[df_pati_data['type'] == "CLM"]
 print(df_pati_clm_data.show())
+
+
+def process_huge_claims(claim_txt):
+    claim_corpus = []
+    # corpus, with no pre-processing to retrieve the original documents.
+    documents = []
+    # for index_val, txt_val in claim_txt.iteritems():
+    #     # print(txt_val)
+    #     splitup_txt = [s.strip() for s in txt_val.split("***")]
+    #     for txt in splitup_txt:
+    #         claim_corpus.append(pre_process(txt))
+    #         documents.append(txt)
+    blah = claim_txt.toPandas().values
+    # status_paths_with_art_class = claim_txt.flatMap(lambda x: [x['text']]).collect()
+
+    # claim_txt.collect().foreach(claim_corpus.append())
+
+    return claim_corpus, documents
+
+""" Load PATI specific data in terms of claims and compare them """
+print('Attempting to load PATI sub data...')
+start = time()
+# df = pd.read_csv("data/pati_data.csv")
+# claim_txts = df['CLAIM_TXTS']
+# claim_txt_corpus, original_corpus = process_claims(claim_txts)
+claim_txts = df_pati_clm_data
+claim_txt_corpus, original_corpus = process_huge_claims(claim_txts)
+print('Took %.2f seconds to load PATI data.' % (time() - start))
 
 tableList = [x["text"] for x in df_pati_clm_data.rdd.collect()]
 
 
-
-# import zipfile
-# zf = zipfile.ZipFile('data/patent_claims_fulltext.csv.zip')
-# df_scott_clm = spark.read\
-#     .format("com.databricks.spark.csv")\
-#     .option("header", "true")\
-#     .option("inferSchema", "true")\
-#     .load('data/patent_claims_fulltext.csv.zip')
-# # spark.read.option("header", "true").csv("myfile.csv")
-# print('# of rows: ', df_scott_clm.count())
-# print(df_scott_clm.head())
-
 # spark.stop()
 
-
-# print('Attempting to load Google model...')
-# start = time()
-# if not os.path.exists('data/GoogleNews-vectors-negative300.bin.gz'):
-#     raise ValueError("SKIP: You need to download the google news model: https://code.google.com/archive/p/word2vec/")
-# model = KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
-# print('Took %.2f seconds to load the Google model.' % (time() - start))
+print('Attempting to load Google model...')
+start = time()
+if not os.path.exists('data/GoogleNews-vectors-negative300.bin.gz'):
+    raise ValueError("SKIP: You need to download the google news model: https://code.google.com/archive/p/word2vec/")
+model = KeyedVectors.load_word2vec_format('data/GoogleNews-vectors-negative300.bin.gz', binary=True)
+print('Took %.2f seconds to load the Google model.' % (time() - start))
 
 
 def pre_process(sentence):
@@ -96,59 +110,41 @@ def remove_duplicates(words):
     return words
 
 
-def process_claims(claim_txt):
-    claim_corpus = []
-    # corpus, with no pre-processing to retrieve the original documents.
-    documents = []
-    for index_val, txt_val in claim_txt.iteritems():
-        # print(txt_val)
-        splitup_txt = [s.strip() for s in txt_val.split("***")]
-        for txt in splitup_txt:
-            claim_corpus.append(pre_process(txt))
-            documents.append(txt)
+# def process_claims(claim_txt):
+#     claim_corpus = []
+#     # corpus, with no pre-processing to retrieve the original documents.
+#     documents = []
+#     for index_val, txt_val in claim_txt.iteritems():
+#         # print(txt_val)
+#         splitup_txt = [s.strip() for s in txt_val.split("***")]
+#         for txt in splitup_txt:
+#             claim_corpus.append(pre_process(txt))
+#             documents.append(txt)
+#
+#     return claim_corpus, documents
 
-    return claim_corpus, documents
 
-
-# """ Load PATI specific data in terms of claims and compare them """
-# print('Attempting to load PATI sub data...')
-# start = time()
-# df = pd.read_csv("data/pati_data.csv")
-# claim_txts = df['CLAIM_TXTS']
-# claim_txt_corpus, original_corpus = process_claims(claim_txts)
-# print('Took %.2f seconds to load PATI data.' % (time() - start))
-
-# print('Attempting to load Patent Claims Research Dataset...')
-# start = time()
-# import zipfile
-# zf = zipfile.ZipFile('data/patent_claims_fulltext.csv.zip')
-# test_df = pd.read_csv(zf.open('patent_claims_fulltext.csv'), encoding="ISO-8859-1", low_memory=False, header=None, chunksize=1000)
-# test_df2 = pd.concat(test_df, ignore_index=True)
-# print('Took %.2f seconds to load Patent Claims Research Dataset.' % (time() - start))
-# print(test_df2.head())
-
-# # Initialize WmdSimilarity.
-# num_best = 10
-# start = time()
-# instance = WmdSimilarity(claim_txt_corpus, model, num_best=num_best)
-# print('Took %.2f seconds to initialize WMD Similarity Instance.' % (time() - start))
+# Initialize WmdSimilarity.
+num_best = 10
+start = time()
+instance = WmdSimilarity(claim_txt_corpus, model, num_best=num_best)
+print('Took %.2f seconds to initialize WMD Similarity Instance.' % (time() - start))
 
 
 def find_similar_claims(claim):
-    pass
-    # # A query is simply a "look-up" in the similarity class.
-    # print('trying to find similar claims')
-    # sims = instance[claim]
-    # print('found similar claims')
-    #
-    # similar_claims = []
-    # for i in range(num_best):
-    #     score = sims[i][1]
-    #     claim_txt = original_corpus[sims[i][0]]
-    #     print('sim = %.4f' % score)
-    #     print(claim_txt)
-    #     print("")
-    #
-    #     similar_claims.append({'similarity_score': str(score), 'similar_claim': claim_txt})
-    #
-    # return similar_claims
+    # A query is simply a "look-up" in the similarity class.
+    print('trying to find similar claims')
+    sims = instance[claim]
+    print('found similar claims')
+
+    similar_claims = []
+    for i in range(num_best):
+        score = sims[i][1]
+        claim_txt = original_corpus[sims[i][0]]
+        print('sim = %.4f' % score)
+        print(claim_txt)
+        print("")
+
+        similar_claims.append({'similarity_score': str(score), 'similar_claim': claim_txt})
+
+    return similar_claims
